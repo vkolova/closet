805f947383cacf77d18515c5eb11f33ee0414f7c
Veselina Kolova <vmkolova@gmail.com>
2018-07-25 19:20:26 +0300

    v0.2.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const morgan = require('morgan');
 const port = process.env.PORT || 3000;
+const request = require('request');
+
+let STATUS = 'Free';
 
 // app.use(morgan('combined'))
 app.use(morgan('[:date[web]] :method :url :status :res[content-length] - :remote-addr - :response-time ms'));
@@ -12,14 +15,30 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/free', (req, res) => {
+  STATUS = 'Free';
+  res.send('Done!');
+});
+
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', socket => {
   socket.on('update', msg => {
+    STATUS = msg;
     socket.broadcast.emit('update', msg);
   });
+});
+
+io.sockets.on('connection', socket => {
+    socket.emit('update', STATUS);
 });
 
 http.listen(port, () => {
   console.log('Is The Toilet Free is online!');
 });
+
+setInterval(() => {
+  request('http://127.0.0.1', (error, response, body) => {
+    console.log('self request');
+  });
+}, 1000*60*15);
